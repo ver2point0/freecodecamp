b10193ed6ce90263a07eeb87f912d43560dbface
John McIntosh II <jmci10@hotmail.com>
2017-01-11 07:54:00 +0000

    remove btn-default class

--- code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
+++ code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
   $("button").addClass("animated");
   $(".btn").addClass("shake");
   $("#target1").addClass("btn-primary");
+  $("button").removeClass("btn-default");
 });
