b7bd6914699d5ded252b4c8ff50fa990d09fbba8
John McIntosh II <jmci10@hotmail.com>
2017-01-11 07:57:51 +0000

    change element css with jQuery

--- code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
+++ code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
   $("button").addClass("animated");
   $(".btn").addClass("shake");
-  $("#target1").addClass("btn-primary");
+  $("#target3").addClass("btn-primary");
   $("button").removeClass("btn-default");
+  $("#target1").css("color", "red");
 });
