9fbae9820a225307a226700e7ae625d957f3f5be
John McIntosh II <jmci10@hotmail.com>
2017-01-12 02:10:29 +0000

    change text inside target4

--- code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
+++ code/0.-front-end-dev/2.-jQuery/js/from-scratch.js
@@ -1,9 +1,5 @@
 /* global $ */
 $(document).ready(function() {
-  $("button").addClass("animated");
-  $(".btn").addClass("shake");
-  $("#target3").addClass("btn-primary");
-  $("button").removeClass("btn-default");
   $("#target1").css("color", "red");
-  $("#target1").prop("disabled", true);
+  $("#target4").html("<em>#target4</em>");
 });
