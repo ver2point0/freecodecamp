34589a73564c0124c1796a2f287eea34f7699815
John McIntosh II <jmci10@hotmail.com>
2017-01-25 04:59:38 +0000

    acces multidimensional arrays with indices

--- code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
+++ code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
@@ -87,3 +87,5 @@ var mixValueArray = ["string", 1, 'c'];
 var multiDimenArray = [["one", ""], [1, 2], ['a', 'b', 'c']];
 multiDimenArray[0];
 multiDimenArray[0][1] = "two";
+
+var accesMultiArray = multiDimenArray[2][2];
