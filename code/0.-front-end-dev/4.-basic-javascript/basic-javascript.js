87c7049e151cbbb8de3645f0e5b11b74a4d5d041
John McIntosh II <jmci10@hotmail.com>
2017-01-31 06:58:35 +0000

    count cards

--- code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
+++ code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
@@ -381,3 +381,34 @@ function returnEarly(x, y) {
     return undefined;
   }
 }
+
+var count = 0;
+
+function countCards(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count -= 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+  }
+  if (count <= 0) {
+    return count + " Hold";
+  } else {
+    return count + " Bet";
+  }
+}
