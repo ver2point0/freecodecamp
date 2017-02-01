02f5127556b209f7c59afd6b4dd676578e98d893
John McIntosh II <jmci10@hotmail.com>
2017-02-01 18:45:42 +0000

    profile lookup

--- code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
+++ code/0.-front-end-dev/4.-basic-javascript/basic-javascript.js
@@ -520,3 +520,44 @@ while (x < 100) {
   arr.push(x);
   x++;
 }
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+ for (var x = 0; x < contacts.length; x++){
+    if (contacts[x].firstName === firstName) {
+      if (contacts[x].hasOwnProperty(prop)) {
+          return contacts[x][prop];
+      } else {
+          return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
