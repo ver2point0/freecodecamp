b7dafdf9a9edd47f04035b7bb2c029c0cf487a96
John McIntosh II <jmci10@hotmail.com>
2017-02-12 05:04:19 +0000

    chunk array into groups

---
+++ code/0.-front-end-dev/6.-basic-algo-scripting/chunky-monkey.js
@@ -0,0 +1,10 @@
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  var i = 0;
+  
+  while (i < arr.length) {
+    newArr.push(arr.slice(i, i + size));
+    i += size;
+  }
+  return newArr;
+}
