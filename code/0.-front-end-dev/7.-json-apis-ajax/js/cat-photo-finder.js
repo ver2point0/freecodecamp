ace010516924709ba8d31cabca9c61d78764c961
John McIntosh II <jmci10@hotmail.com>
2017-02-22 04:58:10 +0000

    convert JSON to HTML

--- code/0.-front-end-dev/7.-json-apis-ajax/js/cat-photo-finder.js
+++ code/0.-front-end-dev/7.-json-apis-ajax/js/cat-photo-finder.js
@@ -2,7 +2,17 @@
 $(document).ready(function() {
   $("#getMessage").on("click", function() {
     $.getJSON("/json/cats.json", function(json){
-      $(".messsage").html(JSON.stringify(json));
+      var html = "";
+      json.forEach(function(val) {
+        var keys = Object.keys(val);
+        html += "<div class = 'cat'>";
+        keys.forEach(function(key) {
+          html += "<strong>" + key + "</strong>: " +
+          val[key] + "<br>";
+        });
+        html += "</div><br>";
+      });
+      $(".message").html(html);
     });
   });
 });
