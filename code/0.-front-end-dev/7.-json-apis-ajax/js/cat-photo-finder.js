ff7bf1f078280ad62293fb2b54db0975dd223ea2
John McIntosh II <jmci10@hotmail.com>
2017-02-24 04:32:14 +0000

    prefilter json

--- code/0.-front-end-dev/7.-json-apis-ajax/js/cat-photo-finder.js
+++ code/0.-front-end-dev/7.-json-apis-ajax/js/cat-photo-finder.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $("#getMessage").on("click", function() {
      $.getJSON("/json/cats.json", function(json) {
       var html = "";
+      json = json.filter(function(val) {
+        return (val.id !== 1);
+      });
       json.forEach(function(val) {
         html += "<div class = 'cat'>";
         html += "<img src= '" + val.imageLink + "' " +
